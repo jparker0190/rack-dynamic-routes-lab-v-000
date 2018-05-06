a6575628e2bda8d5767a75bd031ec99fd9dd0a45
Joseph Parker <jparker010190@gmail.com>
2018-05-06 03:59:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
 
       if item.nil?
         resp.write "Item not found"
-        resp.Status = 400
+        resp.status = 400
       else
         resp.write.item.price
       end
